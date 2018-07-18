4fbbe00f425f1b44705ab155ddacb87543ba2910
Un <keepintine@gmail.com>
2018-07-18 22:31:10 +0300

    Init project and add basic mechanics

---
+++ QuickSnap_Prototype/Assets/__Scripts/Shot.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]   // Делаем видимым в инспекторе
+public class Shot { // Не наследует MonoBehaviour
+
+    static public List<Shot> shots = new List<Shot>();  // Список все снимков
+    static public string prefsName = "QuickSnap_Shots";
+
+    public Vector3 position;    // Позиция камеры
+    public Quaternion rotation; // Поворот камеры
+    public Vector3 target;  // Куда камера показывает
+
+    // Создаёт запись в одну линию для XML документа
+    public string ToXML() {
+        string ss = "<shot ";
+        ss += "x=\"" + position.x + "\" ";
+        ss += "y=\"" + position.y + "\" ";
+        ss += "z=\"" + position.z + "\" ";
+        ss += "qx=\"" + rotation.x + "\" ";
+        ss += "qy=\"" + rotation.y + "\" ";
+        ss += "qz=\"" + rotation.z + "\" ";
+        ss += "qw=\"" + rotation.w + "\" ";
+        ss += "tx=\"" + target.x + "\" ";
+        ss += "ty=\"" + target.y + "\" ";
+        ss += "tz=\"" + target.z + "\" ";
+        ss += " />";
+
+        return (ss);
+    }
+
+    // Берёт PT_XMLHashtable запись снимка крч, и переводит её в объект класса Shot
+    static public Shot ParseShotXML(PT_XMLHashtable xHT) {
+        Shot sh = new Shot();
+
+        sh.position.x = float.Parse(xHT.att("x"));
+        sh.position.y = float.Parse(xHT.att("y"));
+        sh.position.z = float.Parse(xHT.att("z"));
+        sh.rotation.x = float.Parse(xHT.att("qx"));
+        sh.rotation.y = float.Parse(xHT.att("qy"));
+        sh.rotation.z = float.Parse(xHT.att("qz"));
+        sh.rotation.w = float.Parse(xHT.att("qw"));
+        sh.target.x = float.Parse(xHT.att("tx"));
+        sh.target.y = float.Parse(xHT.att("ty"));
+        sh.target.z = float.Parse(xHT.att("tz"));
+
+        return (sh);
+    }
+
+    // Загружает все выстрелы из PlayerPrefs
+    static public void LoadShots() {
+        // Пустой список снимков
+        shots = new List<Shot>();
+        if (!PlayerPrefs.HasKey(prefsName)) {
+            // Пока ещё нет снимков
+            return;
+        }
+
+        // Полностью достаём XML и переводим его
+        string shotsXML = PlayerPrefs.GetString(prefsName);
+        PT_XMLReader xmlr = new PT_XMLReader();
+        xmlr.Parse(shotsXML);
+
+        // Достаём список всех <shot>s
+        PT_XMLHashList hl = xmlr.xml["xml"][0]["shot"];
+        for (int i = 0; i < hl.Count; i++) {
+            // Делаем сокращение
+            PT_XMLHashtable ht = hl[i];
+            Shot sh = ParseShotXML(ht);
+            // Добавляем в список
+            shots.Add(sh);
+        }
+    }
+
+    // Сохраняем список <Shot>  в PlayerPrefs
+    static public void SaveShots() {
+        string xs = Shot.XML;
+
+        Utils.tr(xs);   // расписываем весь XML в консольку
+
+        // Задаём PlayerPrefs
+        PlayerPrefs.SetString(prefsName, xs);
+
+        Utils.tr("PleyerPrefs." + prefsName + " has been set.");
+    }
+
+    // Конвертируем все выстрелы в XML
+    static public string XML {
+        get {
+            // Начинаем XML строку
+            string xs = "<xml> \n";
+            // Добавляем каждый выстрел как <shot> в XML
+            foreach (Shot sh in Shot.shots) {
+                xs += sh.ToXML() + "\n";
+            }
+            // Добавляем закрывающий таг
+            xs += "</xml>";
+            return (xs);
+        }
+    }
+
+    // Удаляем снимки из списка и из PlayerPrefs
+    static public void DeleteShots() {
+        shots = new List<Shot>();
+        if (PlayerPrefs.HasKey(prefsName)) {
+            PlayerPrefs.DeleteKey(prefsName);
+            Utils.tr("PlayerPrefs." + prefsName + " has been deleted.");
+        } else {
+            Utils.tr("There was no PlayerPrefs." + prefsName + " to delete");
+        }
+    }
+
+    // Заменяем снимок
+    static public void ReplaceShot(int ndx, Shot sh) {
+        // Убеждаемся что действительно есть такой снимок
+        if (shots == null || shots.Count <= ndx) return;
+        // Убираем старый выстрел
+        shots.RemoveAt(ndx);
+        // List<>.Insert() добавляет что то в лист в определённый индекс
+        shots.Insert(ndx, sh);
+
+        Utils.tr("Replaced shot:", ndx, "with", sh.ToXML());
+    }
+
+    // Сравниваем два снимка. 1 - идеально подходит, <0 не подходит
+    public static float Compare(Shot target, Shot test) {
+        // Считаем отклонение камеры и удара от Raycast
+        float posDev = (test.position - target.position).magnitude;
+        float tarDev = (test.target - target.target).magnitude;
+
+        float posAccPct, tarAccPct, posAP2, tarAP2; // Степень точности
+
+        TargetCamera tc = TargetCamera.S;
+
+        // Получаем значение точности, где 1 - идеально, 0 - кое как подходит
+        posAccPct = 1 - (posDev / tc.maxPosDeviation);
+        tarAccPct = 1 - (tarDev / tc.maxTarDeviation);
+
+        // Смягчаем значения чтобы оно было более прощающее
+        posAP2 = Easing.Ease(posAccPct, tc.deviationEasing);
+        tarAP2 = Easing.Ease(tarAccPct, tc.deviationEasing);
+
+        float accuracy = (posAP2 + tarAP2) / 2f;
+
+        // Используем Utils чтобы оформить числа в хорошенькую строку
+        string accText = Utils.RoundToPlaces(accuracy * 100).ToString() + "%";
+        Utils.tr("Position:", posAccPct, posAP2, "Target:", tarAccPct, tarAP2, "Accuracy", accuracy);
+
+        return (accuracy);
+    }
+}
---
+++ QuickSnap_Prototype/Assets/__Scripts/TargetCamera.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetCamera : MonoBehaviour {
+    static public TargetCamera S;
+
+    public bool editMode = true;
+    public Camera fpCamera; // Камера от первого лица
+
+    // Максимальное отклонение в Shot.position
+    public float maxPosDeviation = 1f;
+    // Максимальное отклонение в Shot.target
+    public float maxTarDeviation = 0.5f;
+    // Смягчение для этих отклонений
+    public string deviationEasing = Easing.Out;
+    public float passingAccuracy = 0.7f;
+
+    public bool checkToDeletePlayerPrefs = false;
+
+    public bool ____________;
+
+    public Rect camRectNormal;  // Выбирается из camera.rect
+
+    public int shotNum;
+    public Text shotCounter, shotRating;
+    public RawImage checkMark;
+
+    public Shot lastShot;
+    public int numShots;
+    public Shot[] playerShots;
+    public float[] playerRatings;
+
+    void Awake() {
+        S = this;
+    }
+
+    void Start() {
+        // Находим UI элементы
+        GameObject go = GameObject.Find("ShotCounter");
+        shotCounter = go.GetComponent<Text>();
+
+        go = GameObject.Find("ShotRating");
+        shotRating = go.GetComponent<Text>();
+
+        go = GameObject.Find("_Check_64");
+        checkMark = go.GetComponent<RawImage>();
+        // Прячем марку
+        checkMark.enabled = false;
+
+        // Загружаем все снимки из PlayerPrefs
+        Shot.LoadShots();
+        // Если снимки были
+        if(Shot.shots.Count > 0) {
+            shotNum = 0;
+            ResetPlayerShotsAndRatings();
+            ShowShot(Shot.shots[shotNum]);
+        }
+
+        // Прячем курсор ( Заметка: не работает в Unity Editor, пока игра не максимизированна )
+        Cursor.visible = false;
+
+        camRectNormal = GetComponent<Camera>().rect;
+    }
+
+    void ResetPlayerShotsAndRatings() {
+        numShots = Shot.shots.Count;
+        // Создаём playerShots и playerRatings с базовыми значениями
+        playerShots = new Shot[numShots];
+        playerRatings = new float[numShots];
+    }
+
+    void Update() {
+        Shot sh;
+
+        // Ввод с мышки
+        // ЛКМ или ПКМ
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) {
+            sh = new Shot();
+            // Берём позицию и поворот камеры первого лица
+            sh.position = fpCamera.transform.position;
+            sh.rotation = fpCamera.transform.rotation;
+            // Выстреливаем лучом из камеры и смотрим во что он ударится
+            Ray ray = new Ray(sh.position, fpCamera.transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                sh.target = hit.point;
+            }
+
+            if (editMode) {
+                if (Input.GetMouseButtonDown(0)) {
+                    // ЛКМ делает новый снимок
+                    Shot.shots.Add(sh);
+                    shotNum = Shot.shots.Count - 1;
+                } else if (Input.GetMouseButtonDown(1)) {
+                    // ПКМ заменяет текущий снимок
+                    Shot.ReplaceShot(shotNum, sh);
+                    ShowShot(Shot.shots[shotNum]);
+                }
+                // Обнуляем инфу о игроке в редактирующем моде
+                ResetPlayerShotsAndRatings();
+            } else {
+                // Сверяем этот снимок с текущим выстрелом
+                float acc = Shot.Compare(Shot.shots[shotNum], sh);
+                lastShot = sh;
+                playerShots[shotNum] = sh;
+                playerRatings[shotNum] = acc;
+            }
+
+            // Ставим _TargetCamera для показания Shot
+            // ShowShot(sh);
+
+            Utils.tr(sh.ToXML());
+
+            // Записываем новый снимок
+            //Shot.shots.Add(sh);
+            //shotNum = Shot.shots.Count - 1;
+        }
+
+        // Ввод с клавиатуры
+        // Используем Q и E для выбора снимков
+        // Заметка: Всё из этого будет призывать ошибку если Shot.shots пуст
+        if (Input.GetKeyDown(KeyCode.Q)) {  // ShowShow(Shot.shots[(shotNum--)%Shot.shots.Count])
+            shotNum--;
+            if (shotNum < 0) shotNum = Shot.shots.Count - 1;
+            ShowShot(Shot.shots[shotNum]);
+        }
+        if (Input.GetKeyDown(KeyCode.E)) {
+            shotNum++;
+            if (shotNum >= Shot.shots.Count) shotNum = 0;
+            ShowShot(Shot.shots[shotNum]);
+        }
+        // Если editMode активен и левый шифт нажат...
+        if (editMode && Input.GetKey(KeyCode.LeftShift)) {
+            // Используем Shift-S чтобы сохранить
+            if (Input.GetKeyDown(KeyCode.S)) {
+                Shot.SaveShots();
+            }
+            // Shift-X чтобы вывести XML в консоль
+            if (Input.GetKeyDown(KeyCode.X)) {
+                Utils.tr(Shot.XML);
+            }
+        }
+
+        // Удерживаем Tab чтобы увеличить Target окно
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            Rect fpRect = GameObject.Find("FirstPersonCharacter").GetComponent<Camera>().rect;
+            //= new Rect(fpRect);
+            GetComponent<Camera>().rect = new Rect(fpRect);
+        }
+        if (Input.GetKeyUp(KeyCode.Tab)) {
+            // Возвращаем нормальный размер
+            GetComponent<Camera>().rect = camRectNormal;
+        }
+
+        // Обновляем UI text
+        shotCounter.text = (shotNum + 1).ToString() + " of " + Shot.shots.Count;
+        if (Shot.shots.Count == 0) shotCounter.text = "No shots exist";
+        //shotRating.text = "";
+
+        if (playerRatings.Length > shotNum && playerRatings[shotNum] != null) {
+            float rating = Mathf.Max(Mathf.Round(playerRatings[shotNum] * 100f), 0);
+            shotRating.text = rating.ToString() + "%";
+            checkMark.enabled = (playerRatings[shotNum] > passingAccuracy);
+        } else {
+            shotRating.text = "";
+            checkMark.enabled = false;
+        }
+    }
+
+    public void ShowShot(Shot sh) {
+        // Ставим _TargetCamera
+        transform.position = sh.position;
+        transform.rotation = sh.rotation;
+    }
+
+    // OnDrawGizmos() вызывается в любое время когда Gizmos должны быть нарисованны,
+    // Даше если Unity не играет
+    public void OnDrawGizmos() {
+        List<Shot> shots = Shot.shots;
+        for (int i = 0; i < shots.Count; i++) {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(shots[i].position, 0.5f);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(shots[i].position, shots[i].target);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(shots[i].target, 0.25f);
+        }
+
+        // Если  checkToDeletePlayerPrefs выбранно
+        if (checkToDeletePlayerPrefs) {
+            Shot.DeleteShots(); // Удаляем все выстрелы
+            // Убираем галочку
+            checkToDeletePlayerPrefs = false;
+            shotNum = 0;    // Текущий выстрел = 0
+        }
+
+        // Показываем последнюю попытку игрока снимка
+        if (lastShot != null) {
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(lastShot.position, 0.25f);
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(lastShot.position, lastShot.target);
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(lastShot.target, 0.125f);
+        }
+    }
+}
